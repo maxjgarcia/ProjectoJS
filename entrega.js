ee05df35375e86fc22257a04aae101089e3354eb
maxjgarcia <garcia.maximiliano.jesus@gmail.com>
2023-05-04 16:55:45 -0300

    entrega

--- entrega.js
+++ entrega.js
@@ -1,32 +1,17 @@
+const productName = document.getElementById('productName');
+const productPrice = document.getElementById('productPrice');
+const productList = document.getElementById('productList');
+const productQty = document.getElementById('productQty');
+const submitBtn = document.getElementById('submitBtn')
+
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const productNameValue = productName.value;
+    const productPriceValue = productPrice.value;
 
 
 
-
-// creador obj > array para el reduce
-
-function nuevoArticulo (nombre , precio) {
-    this.articuloNombre = nombre = prompt("ingresar nombre del nuevo articulo");
-    this.articuloPrecio = precio = Number(prompt("ingresar precio del articulo"));
-};
-
-const articulos = new nuevoArticulo();
-
-console.log(articulos);
-
-
-// const articulos = [
-//     {nombre: "x", precio: 100 },
-//     {nombre: "y", precio: 200 },
-//     {nombre: "z", precio: 300 },
-// ];
-
-// // let totalCompra = 0
-
-// let montoFinal = articulos.reduce ((suma, producto) => {
-//     return suma + producto.precio;
-// },0);
-
-// console.log(montoFinal);
-
-
-
+    productList.innerHTML += `<li>${productNameValue}: $${productPriceValue}</li>`
+});
